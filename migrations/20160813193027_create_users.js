b8e10cbfef59dc3c965d36dd56aab7b334b70413
Dax <limeside@gmail.com>
2016-08-13 19:37:24 -0500

    Add migrations for user table. :rabbit:

---
+++ migrations/20160813193027_create_users.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table => {
+    table.increments(),
+    table.string('full_name'),
+    table.string('username'),
+    table.text('avatar_url'),
+    table.text('about_me'),
+    table.string('location'),
+    table.timestamp('created_at').defaultTo(knex.fn.now()),
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
